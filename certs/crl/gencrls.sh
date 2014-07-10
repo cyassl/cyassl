a06cff3270e7182eb96c52fc9202eb257b0aaf95
Kaleb Himes <kaleb@wolfssl.com>
2014-07-10 14:33:14 -0600

    kaleb specific changes do not pull in

--- certs/crl/gencrls.sh
+++ certs/crl/gencrls.sh
@@ -5,30 +5,30 @@
 
 
 ######## caCrl ######## 
-#-opt-# openssl ca -gencrl -crldays 365 -out crl.pem -keyfile ~/cyassl/certs/ca-key.pem -cert ~/cyassl/certs/ca-cert.pem
+openssl ca -gencrl -crldays 365 -out crl.pem -keyfile ~/work/my_cyassl_git/certs/ca-key.pem -cert ~/work/my_cyassl_git/certs/ca-cert.pem
 
             ## metadata ##
-#-opt-# openssl crl -in crl.pem -text > tmp
-#-opt-# mv tmp crl.pem
+openssl crl -in crl.pem -text > tmp
+mv tmp crl.pem
             ## install ##
-#-opt-# cp crl.pem ~/cyassl/certs/crl/crl.pem
+cp crl.pem ~/cyassl/certs/crl/crl.pem
 
 
 ######## caCrl server revoked ########
-#-opt-# openssl ca -revoke ~/cyassl/certs/server-cert.pem -keyfile ~/cyassl/certs/ca-key.pem -cert ~/cyassl/certs/ca-cert.pem
+openssl ca -revoke ~/work/my_cyassl_git/certs/server-cert.pem -keyfile ~/work/my_cyassl_git/certs/ca-key.pem -cert ~/work/my_cyassl_git/certs/ca-cert.pem
 
 
 ######## caCrl server revoked generation ########
-#-opt-# openssl ca -gencrl -crldays 365 -out crl.revoked -keyfile ~/cyassl/certs/ca-key.pem -cert ~/cyassl/certs/ca-cert.pem
+openssl ca -gencrl -crldays 365 -out crl.revoked -keyfile ~/work/my_cyassl_git/certs/ca-key.pem -cert ~/work/my_cyassl_git/certs/ca-cert.pem
 
             ## metadata ##
-#-opt-# openssl crl -in crl.revoked -text > tmp
-#-opt-# mv tmp crl.revoked
+openssl crl -in crl.revoked -text > tmp
+mv tmp crl.revoked
             ## install ##
-#-opt-# cp crl.revoked ~/cyassl/certs/crl/crl.revoked
+cp crl.revoked ~/cyassl/certs/crl/crl.revoked
 
             ## remove revoked so next time through the normal CA won't have server revoked ##
-#-opt-# cp blank.index.txt demoCA/index.txt
+cp blank.index.txt demoCA/index.txt
 
 ######## cliCrl ########
 openssl ca -gencrl -crldays 365 -out cliCrl.pem -keyfile ~/work/my_cyassl_git/certs/client-key.pem -cert ~/work/my_cyassl_git/certs/client-cert.pem
