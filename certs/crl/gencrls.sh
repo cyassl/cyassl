a000cae9eb1951e2a63391e5ae8a5cd0348f9985
Kaleb Himes <kaleb@wolfssl.com>
2014-07-11 06:57:59 -0600

    text bodies not right but all certs pass

--- certs/crl/gencrls.sh
+++ certs/crl/gencrls.sh
@@ -11,7 +11,7 @@ openssl ca -gencrl -crldays 365 -out crl.pem -keyfile ~/work/my_cyassl_git/certs
 openssl crl -in crl.pem -text > tmp
 mv tmp crl.pem
             ## install ##
-cp crl.pem ~/cyassl/certs/crl/crl.pem
+cp crl.pem ~/work/my_cyassl_git/certs/crl/crl.pem
 
 
 ######## caCrl server revoked ########
@@ -22,10 +22,10 @@ openssl ca -revoke ~/work/my_cyassl_git/certs/server-cert.pem -keyfile ~/work/my
 openssl ca -gencrl -crldays 365 -out crl.revoked -keyfile ~/work/my_cyassl_git/certs/ca-key.pem -cert ~/work/my_cyassl_git/certs/ca-cert.pem
 
             ## metadata ##
-openssl crl -in crl.revoked -text > tmp
+#openssl crl -in crl.revoked -text > tmp
 mv tmp crl.revoked
             ## install ##
-cp crl.revoked ~/cyassl/certs/crl/crl.revoked
+cp crl.revoked ~/work/my_cyassl_git/certs/crl/crl.revoked
 
             ## remove revoked so next time through the normal CA won't have server revoked ##
 cp blank.index.txt demoCA/index.txt
@@ -37,23 +37,23 @@ openssl ca -gencrl -crldays 365 -out cliCrl.pem -keyfile ~/work/my_cyassl_git/ce
 openssl crl -in cliCrl.pem -text > tmp
 mv tmp cliCrl.pem
             ## install ##
-cp cliCrl.pem ~/cyassl/certs/crl/cliCrl.pem
+cp cliCrl.pem ~/work/my_cyassl_git/certs/crl/cliCrl.pem
 
 ####### eccCliCRL ########
-#-opt-# openssl ca -gencrl -crldays 365 -out eccCliCRL.pem -keyfile ~/cyassl/certs/ecc-client-key.pem -cert ~/cyassl/certs/client-ecc-cert.pem
+openssl ca -gencrl -crldays 365 -out eccCliCRL.pem -keyfile ~/cyassl/certs/ecc-client-key.pem -cert ~/cyassl/certs/client-ecc-cert.pem
 
             ## metadata ##
-#-opt-# openssl crl -in eccCliCRL.pem -text > tmp
-#-opt-# mv tmp eccCliCRL.pem
+openssl crl -in eccCliCRL.pem -text > tmp
+mv tmp eccCliCRL.pem
             ## install ##
-#-opt-# cp eccCliCRL.pem ~/cyassl/certs/crl/eccCliCRL.pem
+cp eccCliCRL.pem ~/cyassl/certs/crl/eccCliCRL.pem
 
 ######## eccSrvCRL ########
-#-opt-# openssl ca -gencrl -crldays 365 -out eccSrvCRL.pem -keyfile ~/cyassl/certs/ecc-key.pem -cert ~/cyassl/certs/server-ecc.pem
+openssl ca -gencrl -crldays 365 -out eccSrvCRL.pem -keyfile ~/cyassl/certs/ecc-key.pem -cert ~/cyassl/certs/server-ecc.pem
 
             ## metadata ##
-#-opt-# openssl crl -in eccSrvCRL.pem -text > tmp
-#-opt-# mv tmp eccSrvCRL.pem
+openssl crl -in eccSrvCRL.pem -text > tmp
+mv tmp eccSrvCRL.pem
             ## install ##
-#-opt-# cp eccSrvCRL.pem ~/cyassl/certs/crl/eccSrvCRL.pem
+cp eccSrvCRL.pem ~/cyassl/certs/crl/eccSrvCRL.pem
 
